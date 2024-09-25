419ac48fcf070790cd56db0d4c9bdc80067e1c0d
lostbefore <lostbefore1129@gmail.com>
2024-09-25 15:59:24 +0800

    Update abca.asm

--- Loop/abca.asm
+++ Loop/abca.asm
@@ -1,35 +1,42 @@
-.MODEL SMALL
-.STACK 100h
-.DATA
-    newline DB 0Dh, 0Ah, '$'    
+STKSEG SEGMENT STACK
+    DW 32 DUP(0)
+STKSEG ENDS
 
-.CODE
-main PROC
-    MOV AX, @DATA                  
+DATASEG SEGMENT
+    newline DB 0Dh, 0Ah, '$'    
+DATASEG ENDS
+
+CODESEG SEGMENT
+    ASSUME CS:CODESEG, DS:DATASEG
+MAIN PROC FAR
+    MOV AX, DATASEG       
     MOV DS, AX
 
-    MOV CX, 26                  
-    MOV AL, 'a'                 
-    MOV BX, 13                  
+    MOV CX, 26            ; CX = 26，输出 26 个小写字母
+    MOV AL, 'a'           ; AL 初始化为 'a'
+    MOV BX, 13            ; BX = 13，每行输出 13 个字符
 
-L:
-    MOV DL, AL                  
-    MOV AH, 2                   
-    INT 21h                     
+print_loop:
+    MOV DL, AL            ; 将当前字母放入 DL
+    MOV AH, 2             ; DOS 中断功能号 2：显示字符
+    INT 21H               ; 调用中断输出字符
 
-    INC AL                      
-    DEC BX                      
-    JNZ continue_print          
+    INC AL                ; AL 加 1，指向下一个字母
+    DEC BX                ; 每输出一个字符，BX 减 1
+    JNZ continue_print    ; 如果还没输出 13 个字符，继续
+    ; 输出换行符
+    MOV AH, 9             ; DOS 中断功能号 9：显示字符串
+    LEA DX, newline       ; 指向换行符
+    INT 21H               ; 输出换行符
 
-    MOV AH, 9                   
-    LEA DX, newline             
-    INT 21h
+    MOV BX, 13            ; 重置 BX 为 13，继续输出下一行
 
-    MOV BX, 13                  
+continue_print:
+    LOOP print_loop       ; 循环输出直到 CX=0
 
-    LOOP L             
-
-    MOV AH, 4Ch                 
-    INT 21h                     
-main ENDP
-END main
+    ; 程序结束
+    MOV AX, 4C00H         ; 程序结束
+    INT 21H
+MAIN ENDP
+CODESEG ENDS
+    END MAIN
