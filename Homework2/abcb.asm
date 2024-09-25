d65d0d09faadba239932d3a6fbe2167064683770
lostbefore <lostbefore1129@gmail.com>
2024-09-25 16:33:05 +0800

    2

---
+++ Homework2/abcb.asm
@@ -0,0 +1,45 @@
+STKSEG SEGMENT STACK
+    DW 32 DUP(0)
+STKSEG ENDS
+
+DATASEG SEGMENT
+    newline DB 0Dh, 0Ah, '$'    ; 换行符
+DATASEG ENDS
+
+CODESEG SEGMENT
+    ASSUME CS:CODESEG, DS:DATASEG
+MAIN PROC FAR
+    MOV AX, DATASEG       
+    MOV DS, AX
+
+    
+    MOV CX, 26            
+    MOV AL, 'a'           
+    MOV BX, 13            
+
+A:                         
+    MOV DL, AL           
+    MOV AH, 2             
+    INT 21H               
+
+    INC AL                
+    DEC BX                
+    JNZ C                 
+
+    
+    MOV AH, 9            
+    LEA DX, newline      
+    INT 21H               
+
+    MOV BX, 13           
+
+C:                         
+    DEC CX                
+    JNZ A                 
+
+    
+    MOV AX, 4C00H         
+    INT 21H
+MAIN ENDP
+CODESEG ENDS
+    END MAIN
